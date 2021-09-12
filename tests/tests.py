53f4bc0dbefa2d7154b2a6f260c80462e8d2e6fc
G. P. Mueller <gpmueller@outlook.com>
2021-09-12 20:50:04 +0200

    Fix tests and poetry config

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,8 @@ import unittest
 import subprocess
 from pathlib import Path, PurePosixPath
 
+from clang_api_doc import clang_api_doc
+
 TESTS_PATH = Path(__file__).parent.resolve()
 
 SIMPLE_H_DOCS = """
@@ -36,14 +38,13 @@ Returns the pointer to the data in the current state
 
 class TestClangApiDoc(unittest.TestCase):
     def test_simple(self):
-        try:
-            infile = TESTS_PATH / "headers" / "simple.h"
-            outfile = TESTS_PATH / "simple.md"
-            subprocess.check_output(["clang-api-doc", '-i', str(infile), '-o', str(outfile)], shell=True, stderr=subprocess.STDOUT)
-            docs = outfile.read_text()
-            self.assertEqual(docs.strip(), SIMPLE_H_DOCS.strip())
-        except subprocess.CalledProcessError as e:
-            self.fail(f"Could not generate docs. Message:\n{e.output.decode('utf-8').strip()}")
+        infile = TESTS_PATH / "headers" / "simple.h"
+        outfile = TESTS_PATH / "simple.md"
+
+        clang_api_doc.transform_file(infile, outfile)
+
+        docs = outfile.read_text()
+        self.assertEqual(docs.strip(), SIMPLE_H_DOCS.strip())
 
 if __name__ == '__main__':
     unittest.main()
